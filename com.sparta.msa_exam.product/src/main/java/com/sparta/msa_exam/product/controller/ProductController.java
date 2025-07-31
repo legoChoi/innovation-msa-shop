fe889cdc6bb794edb7ea07380bd7b026b652e7a5
legoChoi <chlx159@gmail.com>
2025-07-31 16:27:58 +0900

    :sparkles: feat: Product create

---
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.sparta.msa_exam.product.controller;
+
+import com.sparta.msa_exam.product.dto.request.ProductCreateRequest;
+import com.sparta.msa_exam.product.dto.response.ProductCreateResponse;
+import com.sparta.msa_exam.product.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Product-Service OK";
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductCreateResponse> createProduct(
+            @RequestBody @Valid ProductCreateRequest productCreateRequest
+    ) {
+        ProductCreateResponse response = productService.createProduct(productCreateRequest);
+
+        return ResponseEntity.created(null)
+                .body(response);
+    }
+}
