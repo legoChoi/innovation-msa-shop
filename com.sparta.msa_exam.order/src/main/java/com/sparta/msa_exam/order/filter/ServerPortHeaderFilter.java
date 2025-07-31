354252d6c34b5936658fb3898d16176a77e376dc
legoChoi <chlx159@gmail.com>
2025-08-01 01:39:14 +0900

    :sparkles: feat: [All-Service] add a server port header filter

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/filter/ServerPortHeaderFilter.java
@@ -0,0 +1,24 @@
+package com.sparta.msa_exam.order.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ServerPortHeaderFilter extends OncePerRequestFilter {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Server-Port", serverPort);
+        filterChain.doFilter(request, response);
+    }
+}
