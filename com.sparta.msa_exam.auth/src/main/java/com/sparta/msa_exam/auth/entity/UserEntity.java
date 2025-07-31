eb3da023b4a8c4938e26c7a516eeffbf07bfe44a
legoChoi <chlx159@gmail.com>
2025-07-31 14:42:23 +0900

    :sparkles: feat: add User entity

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/entity/UserEntity.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/entity/UserEntity.java
@@ -1,15 +1,21 @@
 package com.sparta.msa_exam.auth.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
+    @Column(nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 }
