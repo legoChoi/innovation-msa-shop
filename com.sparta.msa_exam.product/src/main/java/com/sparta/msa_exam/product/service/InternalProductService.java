ac41d19fcc34e679dc1bd3fb59353814107f4aef
legoChoi <chlx159@gmail.com>
2025-08-04 15:39:10 +0900

    :sparkles: feat: [Product-Service] exception handling

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/service/InternalProductService.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/service/InternalProductService.java
@@ -3,7 +3,10 @@ package com.sparta.msa_exam.product.service;
 import com.sparta.msa_exam.product.dto.request.ProductIdListRequest;
 import com.sparta.msa_exam.product.dto.response.ProductIdListResponse;
 import com.sparta.msa_exam.product.dto.response.SingleProductIdResponse;
+import com.sparta.msa_exam.product.exception.CustomRuntimeException;
+import com.sparta.msa_exam.product.exception.ExceptionMessage;
 import com.sparta.msa_exam.product.repository.ProductRepository;
+import jakarta.ws.rs.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,10 @@ import java.util.List;
 public class InternalProductService {
 
     private final ProductRepository productRepository;
+
+    public void fail() {
+        throw new CustomRuntimeException(ExceptionMessage.PRODUCT_SERVICE_UNAVAILABLE);
+    }
 
     public ProductIdListResponse validateProductIds(ProductIdListRequest request) {
         // validate
@@ -30,7 +37,7 @@ public class InternalProductService {
 
     private void existsProductById(Long productId) {
         if (!productRepository.existsById(productId)) {
-            throw new RuntimeException("Product NotFound Exception"); // TODO throw Product NotFound exception
+            throw new CustomRuntimeException(ExceptionMessage.PRODUCT_NOT_FOUND);
         }
     }
 }
