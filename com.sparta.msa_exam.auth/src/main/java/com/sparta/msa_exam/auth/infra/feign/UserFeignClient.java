4430e367c4e781a93ad9583567dcb62b3f78ed67
legoChoi <chlx159@gmail.com>
2025-08-07 22:56:24 +0900

    :sparkles: feat: [Auth-Service, User-Service] modify service logics to use UserFeignClient

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/infra/feign/UserFeignClient.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/infra/feign/UserFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", fallbackFactory = UserFeignClientFallbackFactory.class)
 public interface UserFeignClient {
 
     @PostMapping("/feign/users")
